f26e809eeca8a426f3577a3ec445b80472d496b1
Shivam <mishrashivam20197@gmail.com>
2026-03-25 12:21:38 +0530

    container with most water

---
+++ MEDIUM LEVEL/container with most water/maxArea.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class maxArea{
+    public static int maxArea(int[] height){
+        int maxArea = 0;
+        int left = 0;
+        int right = height.length - 1;
+    
+        while(left < right){
+            int width = right - left;
+            int minheight = Math.min(height[left], height[right]);
+            int area = width * minheight;
+            maxArea = Math.max(maxArea, area);
+
+            if(height[left] < height[right]){
+                left++;
+            }else{
+                right--;
+            }
+        }
+        return maxArea;
+    }
+    public static void main(String[] args){
+        int[] height = {1,8,6,2,5,4,8,3,7};
+        System.out.println(maxArea(height));
+    }
+}
