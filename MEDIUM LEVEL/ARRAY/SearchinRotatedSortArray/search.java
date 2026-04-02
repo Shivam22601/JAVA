077ee3624b8288dfe840f39795a0369fbc8ed5fa
Shivam <mishrashivam20197@gmail.com>
2026-04-02 21:48:38 +0530

    search in rotated sort array

---
+++ MEDIUM LEVEL/ARRAY/SearchinRotatedSortArray/search.java
@@ -0,0 +1,29 @@
+public class search{
+     public int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if(nums[mid] == target) {
+                return mid;
+            } 
+            else if(nums[mid] < target) {
+                left = mid + 1;
+            } 
+            else {
+                right = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] nums = {4,5,6,7,0,1,2};
+        int target = 0;
+        search s = new search();
+        int result = s.search(nums, target);
+        System.out.println("Target found at index: " + result);
+        
+    }
+}
