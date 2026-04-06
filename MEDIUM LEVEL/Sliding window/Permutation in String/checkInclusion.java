7b88b8f8a740495092d49edf4373cf1ab363c05a
Shivam <mishrashivam20197@gmail.com>
2026-04-06 21:32:13 +0530

    permutation in string

---
+++ MEDIUM LEVEL/Sliding window/Permutation in String/checkInclusion.java
@@ -0,0 +1,45 @@
+public class checkInclusion{
+    public boolean checkInclusion(String s1, String s2) {
+        if (s1.length() > s2.length()) return false;
+
+        int[] s1Count = new int[26];
+        int[] windowCount = new int[26];
+
+        // frequency of s1
+        for (char c : s1.toCharArray()) {
+            s1Count[c - 'a']++;
+        }
+
+        int left = 0;
+
+        for (int right = 0; right < s2.length(); right++) {
+            // add current char to window
+            windowCount[s2.charAt(right) - 'a']++;
+
+            // maintain window size
+            if (right - left + 1 > s1.length()) {
+                windowCount[s2.charAt(left) - 'a']--;
+                left++;
+            }
+
+            // compare arrays
+            if (matches(s1Count, windowCount)) return true;
+        }
+
+        return false;
+    }
+        private boolean matches(int[] a, int[] b) {
+        for (int i = 0; i < 26; i++) {
+            if (a[i] != b[i]) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        checkInclusion solution = new checkInclusion();
+        String s1 = "ab";
+        String s2 = "eidbaooo";
+        boolean result = solution.checkInclusion(s1, s2);
+        System.out.println(result); // Output: true
+    }
+}
